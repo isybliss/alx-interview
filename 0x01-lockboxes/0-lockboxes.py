7cf7047232340b7ee12fef69ee06df5e4773f775
isybliss <isyblissumunna@gmail.com>
2023-07-07 09:31:09 +0000

    Using another method to solve the problem

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,6 +8,7 @@ def canUnlockAll(boxes):
     Return: True or False
     """
 
+    """
     box_len = len(boxes)
     queue = [0]
     unlocked_box = [1] + [0] * (box_len - 1)
@@ -26,3 +27,18 @@ def canUnlockAll(boxes):
     if 0 in unlocked_box:
         return False
     return True
+    """
+
+    box_len = len(boxes)
+    unlocked = [False] * box_len
+    unlocked[0] = True
+    queue = [0]
+
+    while queue:
+        box_index = queue.pop(0)
+        keys = boxes[box_index]
+        for key in keys:
+            if key < box_len and not unlocked[key]:
+                unlocked[key] = True
+                queue.append(key)
+    return all(unlocked)
