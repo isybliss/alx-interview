4f72e32c0b699692f01bbf5ccdfd4d87df9cb03b
isybliss <isyblissumunna@gmail.com>
2023-07-10 14:48:19 +0000

    minimum number of operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+method that calculates the fewest number of operations needed
+to result in exactly n number of characters in the file.
+"""
+
+def minOperations(n):
+    """
+    calaculate minimum number of operations to return n number of characters
+    n: number of characters to be returned
+    Return: number of operations
+    """
+    if n <= 1:
+        return 0
+
+    no_of_operation = 0
+    operators = 2
+
+    while n > 1:
+        if n % operators == 0:
+            n //= operators
+            no_of_operation += operators
+        else:
+            operators += 1
+    return no_of_operation
